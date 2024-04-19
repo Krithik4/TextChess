103ba3633f3e5c80c2b2212dad5005126c645bd9
Krithik4 <krithiksama02@gmail.com>
2024-04-19 10:30:13 -0500

    rook moves

--- src/Rook.java
+++ src/Rook.java
@@ -7,9 +7,53 @@ public class Rook extends Piece {
 
     public boolean canMoveTo(Board gameBoard, String to){
         Piece[][] board = gameBoard.getChessBoard();
-
-
-        return false;
+        int toC = Board.translations.get(to.substring(0 ,1));
+        int toR = 8 - Integer.parseInt(to.substring(1, 2));
+            if (toR == super.row) {
+                if (toC < super.col) {
+                    for (int i = super.col - 1; i > toC; i--) {
+                        if (board[super.row][i] != null) {
+                            return false;
+                        }
+                    }
+                    if(board[toR][toC] == null ||board[toR][toC].getColor() != this.isWhite) {
+                        return true;
+                    }
+                }
+                if (toC > super.col) {
+                    for (int i = super.col + 1; i < toC; i++) {
+                        if (board[super.row][i] != null) {
+                            return false;
+                        }
+                    }
+                    if(board[toR][toC] == null ||board[toR][toC].getColor() != this.isWhite) {
+                        return true;
+                    }
+                }
+            }
+            if (toC == super.col) {
+                if (toR < super.row) {
+                    for (int i = super.row - 1; i > toR; i--) {
+                        if (board[i][super.col] != null) {
+                            return false;
+                        }
+                    }
+                    if(board[toR][toC] == null ||board[toR][toC].getColor() != this.isWhite) {
+                        return true;
+                    }
+                }
+                if (toR > super.row) {
+                    for (int i = super.row + 1; i < toR; i++) {
+                        if (board[i][super.col] != null) {
+                            return false;
+                        }
+                    }
+                    if(board[toR][toC] == null ||board[toR][toC].getColor() != this.isWhite) {
+                        return true;
+                    }
+                }
+            }
+            return false;
     }
 
     // public String toString(){
