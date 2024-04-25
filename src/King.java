af50cc1525adc479ee159e18da3fa65cc4bea3f8
Krithik4 <krithiksama02@gmail.com>
2024-04-25 09:23:38 -0500

    king moves

--- src/King.java
+++ src/King.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class King extends Piece {
 
     public King(boolean isW, int r, int c){
@@ -16,4 +18,20 @@ public class King extends Piece {
     //         return " " + kingB + " " + reset;
     //     }
     // }
+
+    public boolean canMoveTo(Board gameBoard, String to){
+        Piece[][] board = gameBoard.getChessBoard();
+        HashSet<String> possibleMoves = new HashSet<String>();
+        int[] possibleCols = {super.col, super.col, super.col - 1, super.col - 1, super.col - 1, super.col + 1, super.col + 1, super.col + 1};
+        int[] possibleRows = {super.row - 1, super.row + 1, super.row - 1, super.row, super.row + 1, super.row - 1, super.row, super.row + 1};
+        for (int i = 0; i < possibleRows.length; i++){
+            if (possibleCols[i] >= 0 && possibleCols[i] <= 7 && possibleRows[i] >= 0 && possibleRows[i] <= 7){
+                Piece p = board[possibleRows[i]][possibleCols[i]];
+                if (p == null || p.getColor() != this.isWhite){
+                    possibleMoves.add(Board.revTranslations.get(possibleCols[i]) + String.valueOf(8 - possibleRows[i]));
+                }
+            }
+        }
+        return possibleMoves.contains(to);
+    }
 }
